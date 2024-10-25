19970b3ca9b71de700777a07044bdd38ec1ac29c
fazil-expertlogicsol <fazil@expertlogicsol.com>
2024-10-25 10:13:14 -0700

    client from edit

--- app/Http/Controllers/Dashboard/ClientController.php
+++ app/Http/Controllers/Dashboard/ClientController.php
@@ -47,7 +47,7 @@ class ClientController extends Controller
             'cus_name' => 'required|string|max:255',
             'cus_email' => 'required|email|max:255',
             'cus_dob' => 'required|date',
-            'cus_ss' => 'required|string|max:9',
+            'cus_ss' => 'required|string',
             'martial_status' => 'required|in:1,2,3', // Adjust options based on possible statuses
             'cus_is_child' => 'required|in:1,2',
             // 'child_name' => 'array',
@@ -56,30 +56,30 @@ class ClientController extends Controller
             // 'child_age.*' => 'integer|min:0|max:18',
             'cus_address' => 'required|string|max:255',
             'cus_city' => 'required|string|max:255',
-            'cus_state' => 'required|string|max:2',
-            'cus_zip' => 'required|string|max:5',
-            'cus_phone' => 'required|string|max:20',
+            'cus_state' => 'required|string|max:255',
+            'cus_zip' => 'required|string|max:255',
+            'cus_phone' => 'required|string|max:255',
             'role' => 'required|in:1,2,3', // Adjust options based on possible roles
             'cus_dfc' => 'required|date',
             'cus_con' => 'required|string|max:255',
             'cus_conq' => 'required|in:1,2',
             'cus_sex_off' => 'required|in:1,2',
-            'cus_is_offend_minor' => 'required|in:1,0',
-            'cus_food' => 'required|in:1,0',
-            'cus_cloth' => 'required|in:1,0',
-            'cus_shelter' => 'required|in:1,0',
-            'cus_tra' => 'required|in:1,0',
+            'cus_is_offend_minor' => 'required|in:1,2',
+            'cus_food' => 'required|in:1,2',
+            'cus_cloth' => 'required|in:1,2',
+            'cus_shelter' => 'required|in:1,2',
+            'cus_tra' => 'required|in:1,2',
             'cus_emp' => 'required|in:1,2',
             'cus_extra_income' => 'required|in:1,2',
             'cus_church' => 'required|string|max:255',
             'cus_other_church' => 'nullable|string|max:255',
-            'cus_bcert' => 'required|in:1,0',
+            'cus_bcert' => 'required|in:1,2',
             'cus_born_state' => 'required|string|max:2',
-            'cus_state_id' => 'required|in:1,0',
+            'cus_state_id' => 'required|in:1,2',
             'cus_state_no' => 'nullable|string|max:25',
-            'cus_d_lice' => 'required|in:1,0',
+            'cus_d_lice' => 'required|in:1,2',
             'cus_lice_no' => 'nullable|string|max:255',
-            'cus_ss_card' => 'required|in:1,0',
+            'cus_ss_card' => 'required|in:1,2',
             'cus_ssc_no' => 'nullable|string|max:255',
             'cus_insurace' => 'required|in:1,2',
             'cus_carrier' => 'nullable|string|max:255',
@@ -87,8 +87,8 @@ class ClientController extends Controller
             'cus_grp_no' => 'nullable|string|max:255',
             'cus_more_friends' => 'required|in:1,2',
             'cus_counselor' => 'nullable|string|max:255',
-            'cus_is_inv_rom' => 'required|in:1,0',
-            'cus_is_mental_ill' => 'required|in:1,0',
+            'cus_is_inv_rom' => 'required|in:1,2',
+            'cus_is_mental_ill' => 'required|in:1,2',
             'cus_phy_dis' => 'nullable|string|max:255',
             'cus_comments' => 'nullable|string|max:255',
         ]);
@@ -134,16 +134,7 @@ class ClientController extends Controller
                 'is_offend_minor' => $request->cus_is_offend_minor ?? '',
             ]);
 
-            ClientCriminalHistory::create([
-                'gl_ID' => $client->id ?? '',
-                'role' => $request->role ?? '',
-                'date_of_con' => $request->cus_dfc ?? '',
-                'conviction'=> $request->cus_con ?? '',
-                'conq' => $request->cus_conq ?? '',
-                'is_sex_off' => $request->cus_sex_off ?? '',
-                'is_offend_minor' => $request->cus_is_offend_minor ?? '',
-            ]);
-
+        
             ClientSurvey::create([
                 'gl_ID'  => $client->id ?? '',
                 'is_food' => $request->cus_food ?? '',
@@ -171,6 +162,7 @@ class ClientController extends Controller
                 'mem_id'=> $request->cus_mem_id ?? '',
                 'grp_no'=> $request->cus_grp_no ?? '',
             ]);
+            
             ClientInfo::create([
                 'gl_ID' => $client->id ?? '',
                 'more_friends'=> $request->cus_more_friends,
@@ -182,15 +174,13 @@ class ClientController extends Controller
             ]);
             
             DB::commit(); // Commit the transaction if everything works
-            return redirect()->route('properties.index')->with('success', 'Property updated successfully.');
+            return redirect()->route('clients.index')->with('success', 'client updated successfully.');
         } catch (\Exception $e) {
 
             DB::rollBack(); // Rollback if any operation fails
             dd($e->getMessage());
-            return redirect()->back()->withErrors(['error' => 'An error occurred while updating the property. Please try again.']);
+            return redirect()->back()->withErrors(['error' => 'An error occurred while updating the client. Please try again.']);
         }
-        // Redirect or return a response
-        return redirect()->route('clients.index')->with('success', 'Property created successfully!');
     }
 
     /**
@@ -202,7 +192,7 @@ class ClientController extends Controller
     public function show($id)
     {
         $client = Client::WithAllRelations()->findOrFail($id); // Fetch property by ID
-        dd($client);
+        
         return view('livewire.client.show', compact('client')); // Return edit view
     }
 
@@ -230,7 +220,7 @@ class ClientController extends Controller
             'cus_name' => 'required|string|max:255',
             'cus_email' => 'required|email|max:255',
             'cus_dob' => 'required|date',
-            'cus_ss' => 'required|string|max:9',
+            'cus_ss' => 'required|string',
             'martial_status' => 'required|in:1,2,3', // Adjust options based on possible statuses
             'cus_is_child' => 'required|in:1,2',
             // 'child_name' => 'array',
@@ -239,30 +229,30 @@ class ClientController extends Controller
             // 'child_age.*' => 'integer|min:0|max:18',
             'cus_address' => 'required|string|max:255',
             'cus_city' => 'required|string|max:255',
-            'cus_state' => 'required|string|max:2',
-            'cus_zip' => 'required|string|max:5',
-            'cus_phone' => 'required|string|max:20',
+            'cus_state' => 'required|string|max:255',
+            'cus_zip' => 'required|string|max:255',
+            'cus_phone' => 'required|string|max:255',
             'role' => 'required|in:1,2,3', // Adjust options based on possible roles
             'cus_dfc' => 'required|date',
             'cus_con' => 'required|string|max:255',
             'cus_conq' => 'required|in:1,2',
             'cus_sex_off' => 'required|in:1,2',
-            'cus_is_offend_minor' => 'required|in:1,0',
-            'cus_food' => 'required|in:1,0',
-            'cus_cloth' => 'required|in:1,0',
-            'cus_shelter' => 'required|in:1,0',
-            'cus_tra' => 'required|in:1,0',
+            'cus_is_offend_minor' => 'required|in:1,2',
+            'cus_food' => 'required|in:1,2',
+            'cus_cloth' => 'required|in:1,2',
+            'cus_shelter' => 'required|in:1,2',
+            'cus_tra' => 'required|in:1,2',
             'cus_emp' => 'required|in:1,2',
             'cus_extra_income' => 'required|in:1,2',
             'cus_church' => 'required|string|max:255',
             'cus_other_church' => 'nullable|string|max:255',
-            'cus_bcert' => 'required|in:1,0',
+            'cus_bcert' => 'required|in:1,2',
             'cus_born_state' => 'required|string|max:2',
-            'cus_state_id' => 'required|in:1,0',
+            'cus_state_id' => 'required|in:1,2',
             'cus_state_no' => 'nullable|string|max:25',
-            'cus_d_lice' => 'required|in:1,0',
+            'cus_d_lice' => 'required|in:1,2',
             'cus_lice_no' => 'nullable|string|max:255',
-            'cus_ss_card' => 'required|in:1,0',
+            'cus_ss_card' => 'required|in:1,2',
             'cus_ssc_no' => 'nullable|string|max:255',
             'cus_insurace' => 'required|in:1,2',
             'cus_carrier' => 'nullable|string|max:255',
@@ -270,8 +260,8 @@ class ClientController extends Controller
             'cus_grp_no' => 'nullable|string|max:255',
             'cus_more_friends' => 'required|in:1,2',
             'cus_counselor' => 'nullable|string|max:255',
-            'cus_is_inv_rom' => 'required|in:1,0',
-            'cus_is_mental_ill' => 'required|in:1,0',
+            'cus_is_inv_rom' => 'required|in:1,2',
+            'cus_is_mental_ill' => 'required|in:1,2',
             'cus_phy_dis' => 'nullable|string|max:255',
             'cus_comments' => 'nullable|string|max:255',
         ]);
@@ -285,7 +275,8 @@ class ClientController extends Controller
 
         try {
              // Create the property record
-            $client = Client::create([
+            $client = Client::findOrFail($id);
+            $client->update([
                 'cus_name' => $request->cus_name ?? '',
                 'email' => $request->cus_email ?? '',
                 'cus_dob' => $request->cus_dob ?? '',
@@ -299,15 +290,18 @@ class ClientController extends Controller
                 'phone' => $request->cus_phone ?? '',
             ]);
 
-            foreach($request->child_name as $child){
+            ClientChild::where('gl_ID',$id)->delete();
+
+            foreach($request->child_name as $c_key => $child){
                 ClientChild::create([
                     'gl_ID' => $client->id ?? '',
-                    'child_name' => $child->child_name ?? '',
-                    'child_age' => $child->child_age ?? '',
+                    'child_name' => $request->child_name[$c_key] ?? '',
+                    'child_age' => $request->child_age[$c_key] ?? '',
                 ]);
             }
             
-            ClientCriminalHistory::create([
+            $client_criminal_history = ClientCriminalHistory::where('gl_ID',$id)->first();
+            $client_criminal_history->update([
                 'gl_ID' => $client->id ?? '',
                 'role' => $request->role ?? '',
                 'date_of_con' => $request->cus_dfc ?? '',
@@ -316,18 +310,9 @@ class ClientController extends Controller
                 'is_sex_off' => $request->cus_sex_off ?? '',
                 'is_offend_minor' => $request->cus_is_offend_minor ?? '',
             ]);
-
-            ClientCriminalHistory::create([
-                'gl_ID' => $client->id ?? '',
-                'role' => $request->role ?? '',
-                'date_of_con' => $request->cus_dfc ?? '',
-                'conviction'=> $request->cus_con ?? '',
-                'conq' => $request->cus_conq ?? '',
-                'is_sex_off' => $request->cus_sex_off ?? '',
-                'is_offend_minor' => $request->cus_is_offend_minor ?? '',
-            ]);
-
-            ClientSurvey::create([
+    
+            $client_survey = ClientSurvey::where('gl_ID',$id)->first();
+            $client_survey->update([
                 'gl_ID'  => $client->id ?? '',
                 'is_food' => $request->cus_food ?? '',
                 'is_cloth' => $request->cus_cloth ?? '',
@@ -347,14 +332,18 @@ class ClientController extends Controller
                 'ss_number' => $request->cus_ssc_no ?? '',
             ]);
 
-            ClientsHealthIns::create([
+
+            $clients_health_ins = ClientsHealthIns::where('gl_ID',$id)->first();
+            $clients_health_ins->update([
                 'gl_ID' => $client->id ?? '',
                 'is_health'=> $request->cus_insurace ?? '',
                 'carrier'=> $request->cus_carrier ?? '',
                 'mem_id'=> $request->cus_mem_id ?? '',
                 'grp_no'=> $request->cus_grp_no ?? '',
             ]);
-            ClientInfo::create([
+
+            $client_info = ClientInfo::where('gl_ID',$id)->first();
+            $client_info->update([
                 'gl_ID' => $client->id ?? '',
                 'more_friends'=> $request->cus_more_friends,
                 'counselor'=> $request->cus_counselor,
@@ -365,15 +354,13 @@ class ClientController extends Controller
             ]);
             
             DB::commit(); // Commit the transaction if everything works
-            return redirect()->route('properties.index')->with('success', 'Property updated successfully.');
+            return redirect()->route('clients.index')->with('success', 'clients updated successfully.');
         } catch (\Exception $e) {
 
             DB::rollBack(); // Rollback if any operation fails
             dd($e->getMessage());
-            return redirect()->back()->withErrors(['error' => 'An error occurred while updating the property. Please try again.']);
+            return redirect()->back()->withErrors(['error' => 'An error occurred while updating the clients. Please try again.']);
         }
-        // Redirect or return a response
-        return redirect()->route('clients.index')->with('success', 'Property created successfully!');
     }
 
     /**
