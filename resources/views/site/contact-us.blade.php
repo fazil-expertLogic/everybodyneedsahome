e2c15ed800a032f5b0b2b5bb790b61403b827d14
fazil-expertlogicsol <fazil@expertlogicsol.com>
2024-11-08 21:55:58 +0500

    contact-us

--- resources/views/site/contact-us.blade.php
+++ resources/views/site/contact-us.blade.php
@@ -1,15 +1,17 @@
-<?php $page = 'contact-us'; ?>
+<?php $page = 'contact-us'; 
+use App\Helpers\Helper;
+?>
 @extends('site.layout.mainlayout')
 @section('content')
     @component('site.components.breadcrumb')
         @slot('title')
-            Contact Us
+        {!! Helper::pageContent('contact-us','contact-us title') !!}
         @endslot
         @slot('li_1')
-            Home
+        {!! Helper::pageContent('contact-us','contact-us li_1') !!}
         @endslot
         @slot('li_2')
-            Contact Us
+        {!! Helper::pageContent('contact-us','contact-us li_2') !!}
         @endslot
     @endcomponent
 
@@ -23,20 +25,18 @@
                             <div class="col-lg-12 d-flex">
                                 <div class="card">
                                     <div class="card-body contact-info flex-fill">
-                                        <h3>Talk to Member of Sales Team</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis ligula eu
-                                            lectus vulputate porttitor sed feugiat nunc. Mauris ac consectetur ante,</p>
-                                        <a href="javascript:void(0);" class="btn-primary d-inline-block">Contact Sales</a>
+                                        <h3>{!! Helper::pageContent('contact-us','contact-section-h3_1') !!}</h3>
+                                        <p>{!! Helper::pageContent('contact-us','contact-section-p_1') !!}</p>
+                                        <a href="javascript:void(0);" class="btn-primary d-inline-block">{!! Helper::pageContent('contact-us','contact-section-a_1') !!}</a>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-lg-12 d-flex">
                                 <div class="card">
                                     <div class="card-body contact-info flex-fill">
-                                        <h3>Product & Account Support</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis ligula eu
-                                            lectus vulputate porttitor sed feugiat nunc. Mauris ac consectetur ante,</p>
-                                        <a href="{{ url('faq') }}" class="btn-primary d-inline-block">Go to FAQ</a>
+                                        <h3>{!! Helper::pageContent('contact-us','contact-section-h3_2') !!}</h3>
+                                        <p>{!! Helper::pageContent('contact-us','contact-section-p_2') !!}</p>
+                                        <a href="{{ url('faq') }}" class="btn-primary d-inline-block">{!! Helper::pageContent('contact-us','contact-section-a_2') !!}</a>
                                     </div>
                                 </div>
                             </div>
@@ -60,25 +60,25 @@
                         @csrf
                         <div class="card">
                             <div class="card-header">
-                                <h3>Get In Touch</h3>
+                                <h3>{!! Helper::pageContent('contact-us','contact-info-sec-h3') !!}</h3>
                             </div>
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-12 col-12">
                                         <div class="form-group">
-                                            <label>Your Name</label>
+                                            <label>{!! Helper::pageContent('contact-us','contact-info-sec-label_1') !!}</label>
                                             <input type="text" name="name" class="form-control" placeholder="Your Name">
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-12">
                                         <div class="form-group">
-                                            <label>Phone Number</label>
+                                            <label>{!! Helper::pageContent('contact-us','contact-info-sec-label_2') !!}</label>
                                             <input type="text" name="phone" class="form-control" placeholder="Enter Number">
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-12">
                                         <div class="form-group">
-                                            <label>Email Address</label>
+                                            <label>{!! Helper::pageContent('contact-us','contact-info-sec-label_3') !!}</label>
                                             <input type="email" name="email" class="form-control" placeholder="Enter Email">
                                         </div>
                                     </div>
@@ -97,33 +97,33 @@
                                     </div> --}}
                                     <div class="col-lg-6 col-12">
                                         <div class="form-group">
-                                            <label>Subject</label>
+                                            <label>{!! Helper::pageContent('contact-us','contact-info-sec-label_4') !!}</label>
                                             <input type="text" name="subject" class="form-control" placeholder="Enter Subject">
                                         </div>
                                     </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Description</label>
+                                            <label>{!! Helper::pageContent('contact-us','contact-info-sec-label_5') !!}</label>
                                             <textarea class="form-control" name="comment" rows="14" placeholder="Comments"></textarea>
                                         </div>
                                     </div>
                                 </div>
-                                <button type="submit" class="btn-primary">Submit Enquiry</button>
+                                <button type="submit" class="btn-primary">{!! Helper::pageContent('contact-us','contact-info-sec-btn') !!}</button>
                             </div>
                         </div>
                     </form>
                 </div>
                 <div class="col-lg-6">
-                    <h3>Contact Details</h3>
+                    <h3>{!! Helper::pageContent('contact-us','contact-info-sec-h3_1') !!}</h3>
                     <div class="row">
                         <div class="col-lg-12 d-flex">
                             <div class="flex-fill">
                                 <div class="contact-info-details d-flex align-items-center">
                                     <span><img src="{{ URL::asset('/assets/img/icons/phone.svg') }}" alt="Image"></span>
                                     <div>
-                                        <h4>Call Us At</h4>
-                                        <a href="tel:+16398098393">+1 63980 98393</a>,
-                                        <a href="tel:+16398098393">+1 63980 98393</a>
+                                        <h4>{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-h4') !!}</h4>
+                                        <a href="tel:{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-tel1') !!}">{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-tel1') !!}</a>,
+                                        <a href="tel:{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-tel2') !!}">{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-tel2') !!}</a>
                                     </div>
                                 </div>
                             </div>
@@ -133,8 +133,8 @@
                                 <div class="contact-info-details d-flex align-items-center">
                                     <span><img src="{{ URL::asset('/assets/img/icons/mail.svg') }}" alt="Image"></span>
                                     <div>
-                                        <h4>Email Us</h4>
-                                        <a href="mailto:info@example.com">info@example.com</a>
+                                        <h4>{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-h4_1') !!}</h4>
+                                        <a href="mailto:{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-a') !!}">{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-a') !!}</a>
                                     </div>
                                 </div>
                             </div>
@@ -145,15 +145,15 @@
                                     <span><img src="{{ URL::asset('/assets/img/icons/map-pin.svg') }}"
                                             alt="Image"></span>
                                     <div>
-                                        <h4>Location</h4>
-                                        <p>Jl. Surabaya Jaya, No.10, Surabaya United States</p>
+                                        <h4>{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-h4_2') !!}</h4>
+                                        <p>{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-p_1') !!}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="map-location">
-                        <h3>Find Us On</h3>
+                        <h3>{!! Helper::pageContent('contact-us','contact-info-sec contact-info-details-h3') !!}</h3>
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2967.8862835683544!2d-73.98256668525309!3d41.93829486962529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89dd0ee3286615b7%3A0x42bfa96cc2ce4381!2s132%20Kingston%20St%2C%20Kingston%2C%20NY%2012401%2C%20USA!5e0!3m2!1sen!2sin!4v1670922579281!5m2!1sen!2sin"
                             height="359"></iframe>
